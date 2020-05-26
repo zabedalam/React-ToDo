8ce7a34bb94d97aa4ac25d354abda0e3ddfc0941
zabed <zabedalam777@gmail.com>
2020-05-26 15:57:14 +0200

    first commit with skeleton done

---
+++ src/components/ToDoInput.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ToDoInput extends Component {
+  render() {
+    return (
+      <>
+        <div>hello</div>
+      </>
+    )
+  }
+}
+
+export default ToDoInput
